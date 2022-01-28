333a25a50e9b56db066646cb18fa9602ee07c20d
LMAJUCHANC <you@example.com>
2022-01-27 21:41:44 -0600

    Cambios Login

--- pages/index.vue
+++ pages/index.vue
@@ -1,83 +1,42 @@
-<template>
-  <v-row justify="center" align="center">
-    <v-col cols="12" sm="8" md="6">
-      <v-card class="logo py-4 d-flex justify-center">
-        <NuxtLogo />
-        <VuetifyLogo />
-      </v-card>
-      <v-card>
-        <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
-        </v-card-title>
-        <v-card-text>
-          <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
-          <p>
-            For more information on Vuetify, check out the <a
-              href="https://vuetifyjs.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              documentation
-            </a>.
-          </p>
-          <p>
-            If you have questions, please join the official <a
-              href="https://chat.vuetifyjs.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="chat"
-            >
-              discord
-            </a>.
-          </p>
-          <p>
-            Find a bug? Report it on the github <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="contribute"
-            >
-              issue board
-            </a>.
-          </p>
-          <p>Thank you for developing with Vuetify and I look forward to bringing more exciting features in the future.</p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3">
-          <a
-            href="https://nuxtjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt Documentation
-          </a>
-          <br>
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt GitHub
-          </a>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn
-            color="primary"
-            nuxt
-            to="/inspire"
-          >
-            Continue
-          </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-col>
-  </v-row>
-</template>
 
-<script>
-export default {
-  name: 'IndexPage'
-}
+<template>
+  <div id="app">
+  <v-app id="inspire">
+    <div class="pa-0" style="max-height:100vh;overflow:hidden;">
+      <v-img src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+        height="200px" width="100vw"
+      ></v-img>
+    <v-card class="mx-auto elevation-5 text-center pa-5" max-width="350" style="border-radius:20px;position:relative;top:-150px;"
+    >
+      <v-card-subtitle class="title white--text pa-1">
+        Iniciar
+      </v-card-subtitle>
+      <v-card-subtitle class="pa-1">
+        Empleados<br/> Compras/Ventas
+      </v-card-subtitle>
+      <v-text-field hide-details color="red" label="Usuario"></v-text-field>
+      <v-text-field color="red" label="contraseña" type="password"></v-text-field>
+      <v-card-actions>  
+        <v-btn color="red" dark block rounded >
+          Iniciar
+        </v-btn>
+      </v-card-actions>
+        <span class="mt-2 subtitle-2 primary--text" style="cursor:pointer">Olvide Contraseña</span>
+    </v-card>
+      <div class="text-center" style="position:relative;top:-140px">
+        <p class="ma-0">- Buscanos -</p>
+        <v-btn color="grey" rounded outlined class="ma-2" width="200"><v-icon left color="blue">mdi-facebook</v-icon>Facebook</v-btn><br/>        
+        <v-btn color="grey" rounded outlined class="ma-2" width="200"><v-icon left color="red">mdi-google</v-icon>Google</v-btn>
+         <!-- <p class="mt-3">У вас нет аккаунта? <span class="red--text" style="cursor:pointer">Регистрация</span></p> -->
+     </div>
+      </div>
+  </v-app>
+</div>
+</template>
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+    name: 'IndexPage',
+
+})
 </script>
